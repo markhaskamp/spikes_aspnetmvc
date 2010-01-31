6372fe848d4f2917c1b7aa65caf6facfcf8a32f2
Mark Haskamp <mark.haskamp@pinnsg.com>
2010-01-31 14:38:12 -0500

    added Books.cs and BookRepository.cs

---
+++ kindle_books/app/MyKindleBooks/Models/Books.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyKindleBooks.Models
+{
+    public class Books
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
