5eb0725fed19dc720e800a174e39906e8c7e273c
Mark Haskamp <mark.haskamp@pinnsg.com>
2010-02-06 22:24:23 -0500

    now it works

--- kindle_books/app/MyKindleBooks/Domain/Subcategory.cs
+++ kindle_books/app/MyKindleBooks/Domain/Subcategory.cs
@@ -6,6 +6,5 @@ namespace MyKindleBooks.Domain
         virtual public int ProductCategoryID { get; set; }
         virtual public int ProductSubcategoryID { get; set; }
         virtual public string Name { get; set; }
-        virtual public Category category { get; set; }
     }
 }
